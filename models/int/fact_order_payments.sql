a4f435a6ef602b35df196e50f48f09822acea3f4
Merry John <dhfyqmv611@wefre.in>
2023-06-20 11:57:07 +0000

    done changes

--- models/int/fact_order_payments.sql
+++ models/int/fact_order_payments.sql
@@ -1,15 +1,25 @@
-with data as (
-     select 
-     id as order_id,
-    user_id as customer_id,
-     status,
-     order_date
-from {{ref("fct_orders")}}
+with order_details as (
+select
+*
+from {{ref("stg_orders")}}),
+payment_details as (
+select
+*
+from {{ref("stg_payments")}}),
+final as (
+    select
+    a.order_id,
+    b.payment_id,
+    a.customer_id,
+    a.status as order_status,
+    b.status as payment_status,
+    b.payment_method,
+    b.amount as payment_amount,
+    a.order_date,
+    b.created_at as payment_date
+    from order_details as a
+    left join payment_details as b
+    on a.order_id = b.order_id
 )
-select * from data
 
--- customer as (
---     select 
-
--- from {{ref("stg_payments")}}    
--- )
+select * from final
